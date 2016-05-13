592733525def02c1e5be2e29386f19d8bc43345c
Kylie Le <kylie.tram.le@gmail.com>
2016-05-13 14:11:57 -0700

    added api key to dotenv

--- app/controllers/ramen_types_controller.rb
+++ app/controllers/ramen_types_controller.rb
@@ -1,22 +1,22 @@
 class RamenTypesController < ApplicationController
   
   def index
-    @ramen_types = Unirest.get("http://localhost:3000/api/v1/ramen_types.json").body
+    @ramen_types = Unirest.get("#{ENV['API']}.json").body
   end
 
   def show
-    @ramen_type = Unirest.get("http://localhost:3000/api/v1/ramen_types/#{params[:id]}.json").body
+    @ramen_type = Unirest.get("#{ENV['API']}/#{params[:id]}.json").body
   end
 
   def edit
-    @ramen_type = Unirest.get("http://localhost:3000/api/v1/ramen_types/#{params[:id]}.json").body
+    @ramen_type = Unirest.get("#{ENV['API']}/#{params[:id]}.json").body
   end
 
   def new
   end 
 
   def create
-    @ramen_type = Unirest.post("http://localhost:3000/api/v1/ramen_types.json",
+    @ramen_type = Unirest.post("#{ENV['API']}.json",
                   headers:{ "Accept" => "application/json" },
                   parameters: {:name => params[:name], :soup_base => params[:soup_base], :flavor => params[:flavor], :broth_color => params[:broth_color], :other_ingredients => params[:other_ingredients]}).body
 
@@ -24,7 +24,7 @@ class RamenTypesController < ApplicationController
   end
 
   def update
-    @ramen_type = Unirest.patch("http://localhost:3000/api/v1/ramen_types/#{params[:id]}.json",
+    @ramen_type = Unirest.patch("#{ENV['API']}/#{params[:id]}.json",
                   headers:{ "Accept" => "application/json" },
                   parameters: {:name => params[:name], :soup_base => params[:soup_base], :flavor => params[:flavor], :broth_color => params[:broth_color], :other_ingredients => params[:other_ingredients]}).body
 
@@ -32,12 +32,10 @@ class RamenTypesController < ApplicationController
   end
 
   def destroy
-    @ramen_type = Unirest.delete("http://localhost:3000/api/v1/ramen_types/#{params[:id]}.json",
+    @ramen_type = Unirest.delete("#{ENV['API']}/#{params[:id]}.json",
                   headers:{ "Accept" => "application/json" },
                   parameters: {:name => params[:name], :soup_base => params[:soup_base], :flavor => params[:flavor], :broth_color => params[:broth_color], :other_ingredients => params[:other_ingredients]}).body
 
     redirect_to "/ramen_types"
-  end
-
-  
+  end  
 end

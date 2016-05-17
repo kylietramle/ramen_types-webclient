1f13c4f97472148946f5aaa6258f68fb8ddcbfa1
Kylie Le <kylie.tram.le@gmail.com>
2016-05-16 20:45:37 -0700

    DRY code using model methods

--- app/controllers/ramen_types_controller.rb
+++ app/controllers/ramen_types_controller.rb
@@ -1,41 +1,38 @@
 class RamenTypesController < ApplicationController
   
   def index
-    @ramen_types = Unirest.get("#{ENV['API']}.json").body
+    @ramen_types = RamenType.all
   end
 
   def show
-    @ramen_type = Unirest.get("#{ENV['API']}/#{params[:id]}.json").body
+    @ramen_type = RamenType.find(params[:id])
   end
 
   def edit
-    @ramen_type = Unirest.get("#{ENV['API']}/#{params[:id]}.json").body
+    @ramen_type = RamenType.find(params[:id])
   end
 
   def new
   end 
 
   def create
-    @ramen_type = Unirest.post("#{ENV['API']}.json",
-                  headers:{ "Accept" => "application/json" },
-                  parameters: {:name => params[:name], :soup_base => params[:soup_base], :flavor => params[:flavor], :broth_color => params[:broth_color], :other_ingredients => params[:other_ingredients]}).body
+    @ramen_type = RamenType.create(name: params[:name], soup_base: params[:soup_base], flavor: params[:flavor], broth_color: params[:broth_color], other_ingredients:params[:other_ingredients])
 
-    redirect_to "/ramen_types/#{@ramen_type['id']}"
+    redirect_to "/ramen_types/#{@ramen_type.id}"
   end
 
   def update
     @ramen_type = Unirest.patch("#{ENV['API']}/#{params[:id]}.json",
                   headers:{ "Accept" => "application/json" },
-                  parameters: {:name => params[:name], :soup_base => params[:soup_base], :flavor => params[:flavor], :broth_color => params[:broth_color], :other_ingredients => params[:other_ingredients]}).body
+                  parameters: type_params).body
 
-    redirect_to "/ramen_types/#{@ramen_type['id']}"
+    redirect_to "/ramen_types/#{@ramen_type.id}"
   end
 
   def destroy
-    @ramen_type = Unirest.delete("#{ENV['API']}/#{params[:id]}.json",
-                  headers:{ "Accept" => "application/json" },
-                  parameters: {:name => params[:name], :soup_base => params[:soup_base], :flavor => params[:flavor], :broth_color => params[:broth_color], :other_ingredients => params[:other_ingredients]}).body
+    @ramen_type = RamenType.find(params[:id])
+    @ramen_type.destroy
 
     redirect_to "/ramen_types"
-  end  
+  end
 end
---
+++ app/models/ramen_type.rb
@@ -0,0 +1,39 @@
+class RamenType
+  attr_accessor :id, :name, :soup_base, :flavor, :broth_color, :other_ingredients
+
+  def initialize(ramen_type_hash)
+    @id = ramen_type_hash["id"]
+    @name = ramen_type_hash["name"]
+    @soup_base = ramen_type_hash["soup_base"]
+    @flavor = ramen_type_hash["flavor"]
+    @broth_color = ramen_type_hash["broth_color"]
+    @other_ingredients = ramen_type_hash["other_ingredients"]
+  end
+
+  def self.find(id)
+    ramen_type_hash = Unirest.get("#{ENV['API']}/#{id}.json").body
+    return RamenType.new(ramen_type_hash)
+  end
+
+  def self.all
+    ramen_types_array = Unirest.get("#{ENV['API']}.json").body
+
+    ramen_types = []
+    ramen_types_array.each do |hash|
+      ramen_types << RamenType.new(hash)
+    end
+
+    ramen_types
+  end
+
+  def self.create(parameters_hash)
+    ramen_type_hash = Unirest.post("#{ENV['API']}.json",
+                  headers:{ "Accept" => "application/json" },
+                  parameters: parameters_hash).body
+    return RamenType.new(ramen_type_hash)
+  end
+
+  def destroy
+    Unirest.delete("#{ENV['API']}/#{id}.json").body
+  end
+end
